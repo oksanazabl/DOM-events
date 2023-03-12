867a5a20262193b0ef7ab37ed60a95e24cd999e4
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-03-12 13:37:24 +0200

    and task01

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
 const loginButton = document.querySelector(".login-button");
 const modal = document.querySelector(".modal");
+const modalOverlay = document.querySelector(".modal .overlay");
+const modalCloseIcon = document.querySelector(".modal-content .close");
 
-loginButton.addEventListener("click", () => {
-  if (modal.classList.contains("hidden")) {
-    modal.classList.remove("hidden");
-  }
-});
+const isModalVisible = () => !modal.classList.contains("hidden");
+const openModal = () => {
+  if (!isModalVisible()) modal.classList.remove("hidden");
+};
+const closeModal = () => {
+  if (isModalVisible()) modal.classList.add("hidden");
+};
+
+loginButton.addEventListener("click", openModal);
+modalOverlay.addEventListener("click", closeModal);
+modalCloseIcon.addEventListener("click", closeModal);
