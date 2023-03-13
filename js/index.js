8ad1d165db7bd372e643097e0e8fe6983e8fc8aa
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-03-13 23:44:53 +0200

    end

--- js/index.js
+++ js/index.js
@@ -24,3 +24,32 @@ const closeModal = () => {
 loginButton.addEventListener("click", openModal);
 modalOverlay.addEventListener("click", closeModal);
 modalCloseIcon.addEventListener("click", closeModal);
+
+const correctLogin = "user@gmail.com";
+const correctPass = "1111";
+const loginForm = document.querySelector("#login-form");
+const errorInForm = document.querySelector(".error");
+
+const showError = () => {
+  if (errorInForm.classList.contains("hidden")) {
+    errorInForm.classList.remove("hidden");
+  }
+};
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const form = new FormData(e.target);
+
+  if (
+    form.get("login") === correctLogin &&
+    form.get("password") === correctPass
+  ) {
+    closeModal();
+    document.body.innerHTML = `
+        <h2 style="color:green;">Вхід успішний</h2>
+    `;
+  } else {
+    showError();
+  }
+});
